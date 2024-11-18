67f36aab12d13ff220c620a3205f8bff59b26b3c
rakeshrauniyar12 <rakeshrauniyara1234@gmail.com>
2024-11-18 14:12:21 +0530

    product append done

--- gopalnatural/src/Component/Home.js
+++ gopalnatural/src/Component/Home.js
@@ -1,18 +1,441 @@
-import React from 'react';
-import header from "../Assets/Navbar/header.png"
-import "../Style/Home.css"
-const Home = ()=>{
-    return (
-        <div className='home-container'>
-            <div className='header-section'>
-                 <div className='header-content'>
-                    <p className='header-p'>Eat organic food to maintain your health.</p>
-                    <p className='repu-p'>A reputable brand dedicated to providing organic food.</p>
-                    <button className='header-btn'>Shop Now</button>
-                 </div>
-                </div>    
+import React, { useRef, useState } from "react";
+import wish from "../Assets/Navbar/wish.png";
+import { FaRegStar } from "react-icons/fa";
+import meat from "../Assets/Home/meat.png";
+import fruits from "../Assets/Home/fruits.png";
+import milk from "../Assets/Home/milk.png";
+import vegetables from "../Assets/Home/vegetable.png";
+import chicken from "../Assets/Home/product/chicken.png";
+import tomato from "../Assets/Home/product/tomato.png";
+import orange from "../Assets/Home/product/orange.png";
+import mutton from "../Assets/Home/product/mutton.png";
+import milk1 from "../Assets/Home/product/milk.png";
+import curd from "../Assets/Home/product/curd.png";
+import apple from "../Assets/Home/product/apple.png";
+import pr1 from "../Assets/Home/product/pr-1.png";
+import pr2 from "../Assets/Home/product/pr-2.png";
+import pr3 from "../Assets/Home/product/pr-3.png";
+import "../Style/Home.css";
+const Home = () => {
+  return (
+    <div className="home-container">
+      <div className="header-section">
+        <div className="header-content">
+          <p className="header-p">Eat organic food to maintain your health.</p>
+          <p className="repu-p">
+            A reputable brand dedicated to providing organic food.
+          </p>
+          <button className="header-btn">Shop Now</button>
         </div>
-    )
-}
+      </div>
+
+      <div className="choose-categories-1">
+        <div className="choose-content">
+          <div className="choose-heading">
+            <h3>Choose your Categories</h3>
+            <h2>Top Categories</h2>
+          </div>
+          <div className="choose-image">
+            <div>
+              <div>
+                <img src={vegetables} />
+              </div>
+              <p>Vegetables</p>
+            </div>
+            <div>
+              <div>
+                <img src={fruits} />
+              </div>
+              <p>Fruits</p>
+            </div>
+            <div>
+              <div>
+                <img src={milk} />
+              </div>
+              <p>Milk and dairy</p>
+            </div>
+            <div>
+              <div>
+                <img src={meat} />
+              </div>
+              <p>Fresh meat</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="product-categories-1">
+        <div className="choose-content">
+          <div className="choose-heading">
+            <h3>Genuine Organic Goods</h3>
+            <h2>Trending Items</h2>
+          </div>
+          <div className="product-append">
+            {/* first */}
+            <div className="product">
+              <div className="off-div">
+                <p>5 % off</p>
+              </div>
+              <div className="product-image">
+                <img src={apple} alt="Apple" />
+              </div>
+              <p className="productname-size">Apple</p>
+              <p
+                style={{
+                  fontSize: "14px",
+                  fontWeight: "400",
+                  marginTop: "5px",
+                  marginBottom: "5px",
+                }}
+              >
+                (1Kg)
+              </p>
+              <p className="productname-size">
+                <span className="p-span">₹ 120</span>₹ 114
+              </p>
+              <div
+                style={{
+                  display: "flex",
+                  columnGap: "3px",
+                  fontSize: "10px",
+                  marginTop: "5px",
+                  marginBottom: "10px",
+                }}
+              >
+                <FaRegStar />
+                <FaRegStar />
+                <FaRegStar />
+                <FaRegStar />
+                <FaRegStar />
+              </div>
+              <div className="product-last-section">
+                <button className="product-btn">Add to cart</button>
+                <div className="cart-icon-div1">
+                  <img src={wish} />
+                </div>
+              </div>
+            </div>
+
+            {/* second */}
+            <div className="product">
+              <div className="off-div">
+                <p>5 % off</p>
+              </div>
+              <div className="product-image">
+                <img src={chicken} alt="Apple" />
+              </div>
+              <p className="productname-size">Fresh Chicken Meat</p>
+              <p
+                style={{
+                  fontSize: "14px",
+                  fontWeight: "400",
+                  marginTop: "5px",
+                  marginBottom: "5px",
+                }}
+              >
+                (1Kg)
+              </p>
+              <p className="productname-size">
+                <span className="p-span">₹ 120</span>₹ 114
+              </p>
+              <div
+                style={{
+                  display: "flex",
+                  columnGap: "3px",
+                  fontSize: "10px",
+                  marginTop: "5px",
+                  marginBottom: "10px",
+                }}
+              >
+                <FaRegStar />
+                <FaRegStar />
+                <FaRegStar />
+                <FaRegStar />
+                <FaRegStar />
+              </div>
+              <div className="product-last-section">
+                <button className="product-btn">Add to cart</button>
+                <div className="cart-icon-div1">
+                  <img src={wish} />
+                </div>
+              </div>
+            </div>
+
+            {/* third */}
+            <div className="product">
+              <div className="off-div">
+                <p>5 % off</p>
+              </div>
+              <div className="product-image">
+                <img src={orange} alt="Apple" />
+              </div>
+              <p className="productname-size">Orange</p>
+              <p
+                style={{
+                  fontSize: "14px",
+                  fontWeight: "400",
+                  marginTop: "5px",
+                  marginBottom: "5px",
+                }}
+              >
+                (1Kg)
+              </p>
+              <p className="productname-size">
+                <span className="p-span">₹ 120</span>₹ 114
+              </p>
+              <div
+                style={{
+                  display: "flex",
+                  columnGap: "3px",
+                  fontSize: "10px",
+                  marginTop: "5px",
+                  marginBottom: "10px",
+                }}
+              >
+                <FaRegStar />
+                <FaRegStar />
+                <FaRegStar />
+                <FaRegStar />
+                <FaRegStar />
+              </div>
+              <div className="product-last-section">
+                <button className="product-btn">Add to cart</button>
+                <div className="cart-icon-div1">
+                  <img src={wish} />
+                </div>
+              </div>
+            </div>
+
+            {/* four */}
+
+            <div className="product">
+              <div className="off-div">
+                <p>5 % off</p>
+              </div>
+              <div className="product-image">
+                <img src={milk1} alt="Apple" />
+              </div>
+              <p className="productname-size">Fresh Milk</p>
+              <p
+                style={{
+                  fontSize: "14px",
+                  fontWeight: "400",
+                  marginTop: "5px",
+                  marginBottom: "5px",
+                }}
+              >
+                (1 ltr)
+              </p>
+              <p className="productname-size">
+                <span className="p-span">₹ 120</span>₹ 114
+              </p>
+              <div
+                style={{
+                  display: "flex",
+                  columnGap: "3px",
+                  fontSize: "10px",
+                  marginTop: "5px",
+                  marginBottom: "10px",
+                }}
+              >
+                <FaRegStar />
+                <FaRegStar />
+                <FaRegStar />
+                <FaRegStar />
+                <FaRegStar />
+              </div>
+              <div className="product-last-section">
+                <button className="product-btn">Add to cart</button>
+                <div className="cart-icon-div1">
+                  <img src={wish} />
+                </div>
+              </div>
+            </div>
+
+            {/* five */}
+            <div className="product">
+              <div className="off-div">
+                <p>5 % off</p>
+              </div>
+              <div className="product-image">
+                <img src={tomato} alt="Apple" />
+              </div>
+              <p className="productname-size">Tomato</p>
+              <p
+                style={{
+                  fontSize: "14px",
+                  fontWeight: "400",
+                  marginTop: "5px",
+                  marginBottom: "5px",
+                }}
+              >
+                (1Kg)
+              </p>
+              <p className="productname-size">
+                <span className="p-span">₹ 120</span>₹ 114
+              </p>
+              <div
+                style={{
+                  display: "flex",
+                  columnGap: "3px",
+                  fontSize: "10px",
+                  marginTop: "5px",
+                  marginBottom: "10px",
+                }}
+              >
+                <FaRegStar />
+                <FaRegStar />
+                <FaRegStar />
+                <FaRegStar />
+                <FaRegStar />
+              </div>
+              <div className="product-last-section">
+                <button className="product-btn">Add to cart</button>
+                <div className="cart-icon-div1">
+                  <img src={wish} />
+                </div>
+              </div>
+            </div>
+
+            {/* six */}
+            <div className="product">
+              <div className="off-div">
+                <p>5 % off</p>
+              </div>
+              <div className="product-image">
+                <img src={mutton} alt="Apple" />
+              </div>
+              <p className="productname-size">Fresh Mutton</p>
+              <p
+                style={{
+                  fontSize: "14px",
+                  fontWeight: "400",
+                  marginTop: "5px",
+                  marginBottom: "5px",
+                }}
+              >
+                (1Kg)
+              </p>
+              <p className="productname-size">
+                <span className="p-span">₹ 120</span>₹ 114
+              </p>
+              <div
+                style={{
+                  display: "flex",
+                  columnGap: "3px",
+                  fontSize: "10px",
+                  marginTop: "5px",
+                  marginBottom: "10px",
+                }}
+              >
+                <FaRegStar />
+                <FaRegStar />
+                <FaRegStar />
+                <FaRegStar />
+                <FaRegStar />
+              </div>
+              <div className="product-last-section">
+                <button className="product-btn">Add to cart</button>
+                <div className="cart-icon-div1">
+                  <img src={wish} />
+                </div>
+              </div>
+            </div>
+
+            {/* seven  */}
+            <div className="product">
+              <div className="off-div">
+                <p>5 % off</p>
+              </div>
+              <div className="product-image">
+                <img src={curd} alt="Apple" />
+              </div>
+              <p className="productname-size">Fresh Curd</p>
+              <p
+                style={{
+                  fontSize: "14px",
+                  fontWeight: "400",
+                  marginTop: "5px",
+                  marginBottom: "5px",
+                }}
+              >
+                (1Kg)
+              </p>
+              <p className="productname-size">
+                <span className="p-span">₹ 120</span>₹ 114
+              </p>
+              <div
+                style={{
+                  display: "flex",
+                  columnGap: "3px",
+                  fontSize: "10px",
+                  marginTop: "5px",
+                  marginBottom: "10px",
+                }}
+              >
+                <FaRegStar />
+                <FaRegStar />
+                <FaRegStar />
+                <FaRegStar />
+                <FaRegStar />
+              </div>
+              <div className="product-last-section">
+                <button className="product-btn">Add to cart</button>
+                <div className="cart-icon-div1">
+                  <img src={wish} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="product-categories-2">
+        <div className="choose-content">
+          <div className="choose-heading">
+            <h3>Genuine Organic Goods</h3>
+            <h2>Trending Items</h2>
+          </div>
+        </div>
+
+        <div className="product-description">
+            <div className="product-desc-1">
+                <h3 style={{fontSize:"16px",fontWeight:"600"}}>Enjoy a 30% discount on fruit!
+                </h3>
+                <p className="descr-p">Offers fruit combo packs that are packed with fiber and include fruits like banana, kiwi, apple, and pear</p>
+                <div className="img-btn">
+                    <div className="img-btn1"><button className="header-btn1">Shop Now</button></div>
+                    <div className="img-btn2"> <img src={pr2}/></div>
+               
+                   
+                </div>
+            </div>
+            <div className="product-desc-2">
+                <h3 style={{fontSize:"16px",fontWeight:"600"}}>Enjoy a 25% discount on fruit!
+                </h3>
+                <p className="descr-p">Green Brinjal Round, Coriander, Snake Gourd ; Green Brinjal Long, Drumstick, Radish White ; White Brinjal Round, Long Beans, Beetroot.</p>
+                <div className="img-btn">
+                    <div className="img-btn1"><button className="header-btn1">Shop Now</button></div>
+                    <div className="img-btn2"> <img src={pr3}/></div>
+               
+                   
+                </div>
+            </div>
+            <div className="product-desc-3">
+                <h3 style={{fontSize:"16px",fontWeight:"600"}}>Enjoy a 30% discount on fruit!
+                </h3>
+                <p className="descr-p">Offers up to 30% off on meat orders, including chicken, pork, mutton, fish, and seafood. They also offer loyalty discounts and exclusive mutton offers.</p>
+                <div className="img-btn">
+                    <div className="img-btn1"><button className="header-btn1">Shop Now</button></div>
+                    <div className="img-btn2"> <img src={pr1}/></div>
+               
+                   
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
--- gopalnatural/src/Component/Navbar.js
+++ gopalnatural/src/Component/Navbar.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Style/Navbar.css"
 import { IoSearch } from "react-icons/io5";
 import { RiArrowDropDownLine } from "react-icons/ri";
 import wish from "../Assets/Navbar/wish.png"
 import cart_icon from "../Assets/Navbar/cart-icon.png"
-
+import fruit1 from "../Assets/Home/fruits.png"
+import vegetable from "../Assets/Home/vegetable.png"
+import milk from "../Assets/Home/milk.png"
+import meat from "../Assets/Home/meat.png"
 
 const Navbar = ()=>{
+   const  [showCategories,setShowCategories] = useState(false);
+   const  [showCategories1,setShowCategories1] = useState(false);
+
+    const handleCategories = ()=>{
+      setShowCategories(!showCategories);   
+    }
+
+    const handleCategories1 = ()=>{
+        setShowCategories1(!showCategories1);   
+      }
     return(
         <div className="navbar-container">
               <div className="navbar-main">
@@ -42,17 +55,59 @@ const Navbar = ()=>{
                     <div className="navbar-end-content">
                         <div className="first-drop-arrow">
                             <div><p>Browse By Categories</p></div>
-                            <RiArrowDropDownLine style={{fontSize:"30px",color:"black"}}/>
+                            <RiArrowDropDownLine style={{fontSize:"30px",color:"black"}} onClick={handleCategories}/>
                         </div>
                         <div className="second-div-content">
                             <p>Home</p>
-                            <div className="second-drop-arrow"><p>Shop</p><RiArrowDropDownLine style={{fontSize:"30px",color:"#ffff"}}/></div>
+                            <div className="second-drop-arrow"><p>Shop</p><RiArrowDropDownLine style={{fontSize:"30px",color:"#ffff"}} onClick={handleCategories1}/></div>
                             <p>About us</p>
                             <p>Blog</p>
                             <p>Contact us</p>
                         </div>
                         </div> 
               </div>
+
+              {showCategories && 
+                <div className="show-categories">
+                    <div className="show-categories1">
+                        <div className="cate-img"><img src={fruit1}/></div>
+                        <p className="categ-p">Fresh Fruits</p>
+                        </div>
+                        <div className="show-categories1">
+                        <div className="cate-img"><img src={vegetable}/></div>
+                        <p className="categ-p">Fresh Vegetables</p>
+                        </div>
+                        <div className="show-categories1">
+                        <div className="cate-img"><img src={milk}/></div>
+                        <p className="categ-p">Fresh Milk and Dairy</p>
+                        </div>
+                        <div className="show-categories1">
+                        <div className="cate-img"><img src={meat}/></div>
+                        <p className="categ-p">Fresh Meats</p>
+                        </div>
+                </div>
+              }
+
+{showCategories1 && 
+                <div className="show-categories11">
+                    <div className="show-categories1">
+                        <div className="cate-img"><img src={fruit1}/></div>
+                        <p className="categ-p">Fresh Fruits</p>
+                        </div>
+                        <div className="show-categories1">
+                        <div className="cate-img"><img src={vegetable}/></div>
+                        <p className="categ-p">Fresh Vegetables</p>
+                        </div>
+                        <div className="show-categories1">
+                        <div className="cate-img"><img src={milk}/></div>
+                        <p className="categ-p">Fresh Milk and Dairy</p>
+                        </div>
+                        <div className="show-categories1">
+                        <div className="cate-img"><img src={meat}/></div>
+                        <p className="categ-p">Fresh Meats</p>
+                        </div>
+                </div>
+              }
         </div>
     )
 }
